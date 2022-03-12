36c37b3f9172b1bb992a6449207dfeee1e47829a
Kaide <kaidewu@protonmail.com>
2022-03-12 19:07:10 +0100

    Unfinished

---
+++ Stones on the Table/stones_on_the_table.py
@@ -0,0 +1,11 @@
+while True:
+    n = int(input())
+    if n in range(1, 51):
+        break
+while True:
+    s = list(input())
+    if len(s) == n:
+        break
+rgb = ['R', 'G', 'B']
+for i in range(n):
+    pass
